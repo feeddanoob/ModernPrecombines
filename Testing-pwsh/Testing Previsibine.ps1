14e67a45006d250d55ae403df4bc398fad025f58
Noubar Michael Chorbajian <noubarnouba@gmail.com>
2022-04-09 03:01:45 -0700

    Update Testing Previsibine.ps1
    Added Comments and more stuff to the DATA hash table. Substituted the previous write-info and read-host messages with ones from the DATA hash table.

--- Testing-pwsh/Testing Previsibine.ps1
+++ Testing-pwsh/Testing Previsibine.ps1
@@ -1,13 +1,20 @@
 #Min Powershell version.
 #Requires -Version 7.0
+#Parameter blocks to add arguments for the script in case user doesn't want to write on console.
 [CmdletBinding()]
 param (
+    #Not Mandatory on purpose, user should not be forced to run script with arguments when user input is available in console.
     [Parameter()]
+    #Since this is a string input, should not be null or empty.
     [ValidateNotNullOrEmpty()]
+    #Grouped up conditions
     [ValidateScript(
-        { 
+        {
+            #If there are invalid characters such as '<', '>', and '*' in the file name stop and throw the exception
             if ($_.IndexOfAny([System.IO.Path]::GetInvalidFileNameChars()) -eq -1) {
+                #If the name is just .esp return false and throw the exception
                 if (-not $_.Equals(".esp") -and -not $_.Equals(".esm")) {
+                    #Make sure the file name ends with a .esp or .esm extension. Not including .esl's because I am 100% not sure how the CK reacts to it.
                     if ($_.EndsWith(".esp") -or $_.EndsWith(".esm")) {
                         $true
                     } else {
@@ -26,14 +33,17 @@ param (
     [string[]]
     $ESPName,
 
+    #Like above not mandatory incase user wants to write on console but with an addition, have an option to save to a file.
     [Parameter()]
+    #Like above, do not want null or empty
     [ValidateNotNullOrEmpty()]
+    #See if the File exists in the path the user provided.
     [ValidateScript(
         { 
             if (Test-Path -Path $_\FO4Edit.exe) {
                 $true
             } else {
-                throw "Could not find FO4Edit with that file path."
+                throw "Could not find FO4Edit with that file path provided."
             }
         }
     )]
@@ -43,10 +53,6 @@ param (
 )
 #Automatically looks for the FO4 installation path using Registry Keys
 $FO4InstallPath = Get-ItemPropertyValue -Path 'HKLM:\SOFTWARE\WOW6432Node\Bethesda Softworks\Fallout4\' -Name "installed path"
-#Blank ESP/ESM file name, I decided to have the user input the ESP/ESM file for precombine generation in the case there might be multiple files in the DATA folder. 
-#$script:ESPName = $null
-#Blank Path to XEdit folder location, kinda wish there was a way to find XEdit without using Get-ChildItem "C:\" -recurse -ErrorAction:SilentlyContinue -Include "FO4Edit.exe" as that will take forever.
-#$script:PathXEdit = $null
 
 $MessageXEditPath = "Please put in your FO4Edit folder path here"
 
@@ -56,9 +62,9 @@ $MessageInsert = "Insert a valid name for the esp/esm"
 
 #$MessageMovingToFO4 = "Moving to the FO4 Directory"
 
-$MessageCK = "Checking to make sure the CK is in the FO4 root directory"
+#$MessageCK = "Checking to make sure the CK is in the FO4 root directory"
 
-$MessageFO4location = "Please put in your Fallout 4 Directory here"
+#$MessageFO4location = "Please put in your Fallout 4 Directory here"
 
 $Disclaimer = DATA {
     "DISCLAIMER: This script is only to be used after you created the base previsibine ESP/ESM."
@@ -87,6 +93,12 @@ $Messages = DATA {
     ESPXEdit = You chose to use FO4Edit, running FO4Edit now. Please use Searge's 03_MergeCombinedObjects xedit script to your plugin.
     ArchiveMesh = Packing the meshes to an archive.
     NoMesh = Meshes were not generated from the CK, Aborting.
+    CheckCK = Checking to make sure the CK is in FO4's root directory.
+    NoCK = Could not find the Creation Kit in Fallout 4's directory.
+    FO4Loc = Could not find Fallout 4's location using registry key. Please put in your Fallout 4 Directory here.
+    FO4Found = Found Fallout4.exe with the directory provided.
+    CKFound = Found the Creation Kit in the same directory as Fallout4.exe.
+    NOFO4 = Could not find Fallout4.exe with the location provided.
 '@
 }
 
@@ -199,38 +211,35 @@ function ArchiveInit {
         [string]$ESP
     )
     if ([bool](Get-ChildItem -Recurse | Where-Object Name -Like "*.nif")) {
-        Write-Information -MessageData "Packing the meshes to an archive" -InformationAction:Continue
+        Write-Information -MessageData $Messages.ArchiveMesh -InformationAction:Continue
         Start-Process -FilePath "$FO4InstallPath\Tools\Archive2\Archive2.exe" -Wait -ArgumentList """$FO4InstallPath\Data\Meshes"" -c=""$FO4InstallPath\Data\$ESP - Main.ba2"""
     } else {
-        Write-Information -MessageData "Meshes were not created, Aborting" -InformationAction:Stop
+        Write-Information -MessageData $Messages.NoMesh -InformationAction:Stop
     }
 }
 
 Function Get-CK {
-    #Set-Location $FO4InstallPath
-    #Write-Information -MessageData $MessageMovingToFO4 -InformationAction:Continue
-    Write-Information -MessageData $MessageCK -InformationAction:Continue
+    Write-Information -MessageData $Messages.CheckCK -InformationAction:Continue
     if ($TestCKLoc = Test-Path -Path $FO4InstallPath\CreationKit.exe) {
         return $TestCKLoc
     } else {
-        Write-Information -MessageData "Could not find the CK in the Fallout 4 Directory, exiting." -InformationAction:Continue
+        Write-Information -MessageData $Messages.NoCK -InformationAction:Continue
         return 0
     }
 }
 
 Function Set-CK {
     do {
-        $script:FO4InstallPath = Read-Host -Prompt $MessageFO4location
+        $script:FO4InstallPath = Read-Host -Prompt $Messages.FO4Loc
         if ($FO4InstallCheck = Test-Path -Path $FO4InstallPath\Fallout4.exe) {
-            Write-Information -MessageData "Found Fallout4.exe, now looking for the Creation Kit." -InformationAction:Continue
+            Write-Information -MessageData $Messages.FO4Found -InformationAction:Continue
             if (Test-Path -Path $FO4InstallPath\CreationKit.exe) {
-                Write-Information -MessageData "Found the Creation Kit" -InformationAction:Continue
-                #Set-Location $FO4InstallPath
+                Write-Information -MessageData $Messages.CKFound -InformationAction:Continue
             } else {
-                Write-Information -MessageData "Could not find the Creation Kit in the FO4 Directory, please install the Creation Kit in the FO4 directory." -InformationAction:Stop
+                Write-Information -MessageData $Messages.NoCK -InformationAction:Stop
             }
         } else {
-            Write-Information -MessageData "Could not find Fallout4.exe with that directory, please input the correct directory." -InformationAction:Continue
+            Write-Information -MessageData $Messages.NOFO4 -InformationAction:Continue
         }
     } until($FO4InstallCheck)
 }
